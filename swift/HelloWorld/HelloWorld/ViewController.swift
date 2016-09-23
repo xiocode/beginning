cbef3fb4854173175e30e4acaae07c5327af1cc2
Tony.Shao <xiocode@gmail.com>
2016-09-23 14:20:40 +0800

    init

---
+++ swift/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Tony.Shao on 9/23/16.
+//  Copyright © 2016 Tony.Shao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showMessage() {
+        let messageController = UIAlertController(title: "测试", message: "测试一个", preferredStyle: UIAlertControllerStyle.alert)
+        messageController.addAction(UIAlertAction(title: "确认", style: UIAlertActionStyle.default, handler: nil))
+        self.present(messageController, animated: true, completion: nil)
+    }
+
+}
+
